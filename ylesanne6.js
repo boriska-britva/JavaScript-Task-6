d0a93b4595aeb3bc4cf75d041871449cc6971296
Mark VOCO <mark.hardikov@voco.ee>
2026-09-25 10:14:36 +0000

    tehtud

---
+++ ylesanne6.js
@@ -0,0 +1,23 @@
+let booking = 5;
+
+switch (booking) {
+    case 1:
+    case 2:
+        console.log("Valige laud kahele inimesele.");
+        break;
+
+    case 3:
+    case 4:
+        console.log("Valige laud neljale inimesele");
+        break;
+
+    case 5:
+    case 6:
+        console.log("Valige laud kuuele inimesele.");
+        break;
+
+    default:
+
+        console.log("Valige suur laud.");
+        break;
+}
